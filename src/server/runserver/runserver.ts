9f29e2b9fe0a3f518b14d59ac628962c0fd3cda7
cyberkarma <icegroovekid@gmail.com>
2023-09-25 19:02:31 +0500

    Tests

--- src/server/runserver/runserver.ts
+++ src/server/runserver/runserver.ts
@@ -1,15 +1,21 @@
-import express from 'express'
+import express, {Response} from 'express'
 import {videoRouter} from "../../store/video/video-router";
 import bodyParser from "body-parser";
+import {generateVideos} from "../../store/video/video";
 
 
 export function runServer() {
+    const videos = generateVideos(10)
     const app = express()
     const port = 3000
     const bdMiddleware = bodyParser({})
 
    app.use(bdMiddleware)
    app.use('/videos', videoRouter)
+    app.delete('/testing/all-data', (_, res:Response) => {
+        res.send(204)
+
+    })
 
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
