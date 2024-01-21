88ec2f81baaa651edbce79bc4bcfb0a1a8b0e5f8
cyberkarma <icegroovekid@gmail.com>
2024-01-21 15:46:25 +0500

    blog dal

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -1,12 +1,16 @@
 import { validationResult, check, FieldValidationError } from "express-validator";
 import {Request, Response, NextFunction} from "express";
 
+type ValidationResultError = {
+    [string: string]: [string];
+};
+
 // Функция, возвращающая массив проверок
 export const blogValidationRules = () => {
     return [
-        check('name').trim().notEmpty().withMessage('name').isLength({ max: 15 }),
-        check('description').notEmpty().withMessage('description').isLength({ max: 500 }),
-        check('websiteUrl').notEmpty().withMessage('websiteUrl').isURL().withMessage('websiteUrl').isLength({ max: 100 })
+        check('name').trim().notEmpty().withMessage('name !').isLength({ max: 15 }),
+        check('description').notEmpty().withMessage('description!').isLength({ max: 500 }),
+        check('websiteUrl').notEmpty().withMessage('websiteUrl!').isURL().withMessage('websiteUrl!!').isLength({ max: 100 })
     ];
 };
 
@@ -15,30 +19,17 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        const formattedErrors = errors.array({ onlyFirstError: true }).map(error => ({
-            message: error.msg,
-            field: error.msg,
-        }));
+        const formattedErrors = errors.array({ onlyFirstError: true }).map(error => {
+            return {
+                message: error.msg,
+                field: error.type === 'field' && error.path
+            }
+        });
 
         res.status(400).json({ errorsMessages: formattedErrors });
     } else {
         next();
     }
-
-
-
-    // const errors = validationResult(req);
-    //
-    // if (!errors.isEmpty()) {
-    //     const formattedErrors = errors.array({ onlyFirstError: true }).map(error => ({
-    //         message: error.msg,
-    //         field: error.msg,
-    //     }));
-    //
-    //     res.status(400).json({ errorsMessages: formattedErrors });
-    // } else {
-    //     next();
-    // }
 
 
 
