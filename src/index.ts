d471c6416a1fdfa129827a6a4ae8fa6edf9df1e8
cyberkarma <icegroovekid@gmail.com>
2024-02-24 23:54:47 +0500

    mongo dal

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,67 @@
-import {runServer} from "./server/runserver/runserver";
-// import express from "express";
-import express, {Response} from 'express'
+import express, { Express, Response } from 'express';
+import { blogRouter } from './repositories/blogs/blog-router';
+import { postRouter } from './repositories/posts/post-router';
+import { runDb } from './repositories/db';
+import dotenv from 'dotenv';
 
-import dotenv from  'dotenv'
-import {blogRouter} from "./repositories/blogs/blog-router";
-import {postRouter} from "./repositories/posts/post-router";
-import {runDb} from "./repositories/db";
-
-dotenv.config()
+dotenv.config();
 
 export const RouterPaths = {
     blogs: '/blogs',
     posts: '/posts',
-}
+};
 
-
-
-
-export const app = express()
-const port = process.env.PORT || 5000
+const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(RouterPaths.blogs, blogRouter)
-app.use(RouterPaths.posts, postRouter)
-
+app.use(RouterPaths.blogs, blogRouter);
+app.use(RouterPaths.posts, postRouter);
 
 app.delete('/testing/all-data', (_, res: Response) => {
-    res.sendStatus(204)
-})
-const startApp = async () => {
-  await runDb()
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
-    })
-}
-startApp()
+    res.sendStatus(204);
+});
+
+runDb().catch(console.error);
+
+export default app;
+
+// import {runServer} from "./server/runserver/runserver";
+// import express, {Response} from 'express'
+//
+// import dotenv from  'dotenv'
+// import {blogRouter} from "./repositories/blogs/blog-router";
+// import {postRouter} from "./repositories/posts/post-router";
+// import {runDb} from "./repositories/db";
+//
+// dotenv.config()
+//
+// export const RouterPaths = {
+//     blogs: '/blogs',
+//     posts: '/posts',
+// }
+//
+//
+//
+//
+// export const app = express()
+// const port = process.env.PORT || 5000
+//
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(RouterPaths.blogs, blogRouter)
+// app.use(RouterPaths.posts, postRouter)
+//
+//
+// app.delete('/testing/all-data', (_, res: Response) => {
+//     res.sendStatus(204)
+// })
+// const startApp = async () => {
+//   await runDb()
+//     app.listen(port, () => {
+//         console.log(`Example app listening on port ${port}`)
+//     })
+// }
+// startApp()
 
 // runServer(app)
