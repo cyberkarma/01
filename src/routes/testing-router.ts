0c7deedefbe23a3ae8c93202deaa70a0f94e98a7
cyberkarma <icegroovekid@gmail.com>
2024-03-10 17:20:08 +0500

    query/ blogs/id/posts

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -6,8 +6,12 @@ import {blogsCollection, postsCollection} from "../repositories/db";
 export const testingRouter = Router()
 
 testingRouter.delete("/all-data", async (req: Request, res: Response) => {
+    console.log('before')
+    const result2 = await blogsCollection.find({})
+    console.log('before', result2)
     await postsCollection.deleteMany({});
     await blogsCollection.deleteMany({});
-
+   const result = await blogsCollection.find({})
+    console.log('after', result)
     res.sendStatus(204);
 })
